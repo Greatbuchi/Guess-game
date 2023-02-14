bdb98fbef0564f0dceef308ed9ee31e185edc248
greatbuchi <vmbuchi32@gmail.com>
2023-02-13 22:22:25 -0800

    Changes to initial commit

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const highScore = document.querySelector('.highscore');
 const playAgain = document.querySelector('.again');
 const checkGuess = document.querySelector('.check');
 const score = document.querySelector('.score');
+const numberGuessed = document.querySelector('.number');
+const message = document.querySelector('.message');
 
 const numberToGuess = Math.floor(Math.random() * 30);
 
@@ -12,16 +14,19 @@ checkGuess.addEventListener('click', () => {
   let userGuess = Number(guess.value);
   if (typeof userGuess != 'number') return;
 
-  if (userGuess === numberToGuess) handleWin();
-  handleFailedGuesses(userGuess, numberToGuess);
+  handleWinOrFailedGuesses(userGuess, numberToGuess);
 });
 
-function handleFailedGuesses(userGuess, numberToGuess) {
-  if (userGuess < numberToGuess) {
-    console.log('Number is too low');
-  } else if (userGuess > numberToGuess) {
-    console.log('Number is too high');
-  }
+function handleWinOrFailedGuesses(userGuess, numberToGuess) {
+  if (userGuess === numberToGuess) handleWin();
+  if (userGuess < numberToGuess) displayFailedResult(userGuess);
+  if (userGuess > numberToGuess) displayFailedResult(userGuess);
+}
+
+function displayFailedResult(userGuess) {
+  score.innerHTML = Number(score.innerHTML) - 1;
+  guess.value = '';
+  numberGuessed.innerHTML = userGuess;
 }
 
 function handleWin() {
